9f383a2910a67bfe9fc7c3771c397db4ed027716
Luciano Oliveira <luciano101@msn.com>
2017-07-10 16:47:29 -0400

    commit dos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
-import {Col} from 'react-bootstrap';
+import {Col, Button, Collapse, Well} from 'react-bootstrap';
 
 class Projects extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+
+		};
+	}
 	render(){
 		return(
-			<Col xs={12} md={6} lg={6}>Projects</Col>
+			<Col xs={12} md={6} lg={6} className="right-col">
+				<Button onClick={ ()=> this.setState({ open: !this.state.open })}>
+					Project 1
+				</Button>
+				<Collapse in={this.state.open}>
+					<Well>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi architecto maiores adipisci, accusamus aspernatur corrupti hic facilis iure assumenda at? Odit atque, dignissimos dolore modi temporibus error nobis saepe. Minus?
+					</Well>
+				</Collapse>
+			</Col>
 		)
 	}
 }
