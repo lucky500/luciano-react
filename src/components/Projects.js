ad7ca882f1006ae5c8755470452e91ace4cc5c87
Luciano Oliveira <luciano101@msn.com>
2017-07-10 17:12:35 -0400

    commit tres

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col, Button, Collapse, Well} from 'react-bootstrap';
+import {Col, ButtonGroup, Button, Collapse, Well} from 'react-bootstrap';
 
 class Projects extends Component {
 	constructor(props){
@@ -11,14 +11,24 @@ class Projects extends Component {
 	render(){
 		return(
 			<Col xs={12} md={6} lg={6} className="right-col">
-				<Button onClick={ ()=> this.setState({ open: !this.state.open })}>
-					Project 1
-				</Button>
-				<Collapse in={this.state.open}>
-					<Well>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi architecto maiores adipisci, accusamus aspernatur corrupti hic facilis iure assumenda at? Odit atque, dignissimos dolore modi temporibus error nobis saepe. Minus?
-					</Well>
-				</Collapse>
+				<ButtonGroup vertical block>
+					<Button onClick={ ()=> this.setState({ open: !this.state.open })}>
+						Project 1
+					</Button>
+					<Collapse in={this.state.open}>
+						<Well>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi architecto maiores adipisci, accusamus aspernatur corrupti hic facilis iure assumenda at? Odit atque, dignissimos dolore modi temporibus error nobis saepe. Minus?
+						</Well>
+					</Collapse>
+					<Button onClick={ ()=> this.setState({ open: !this.state.open })}>
+						Project 2
+					</Button>
+					<Collapse in={this.state.open}>
+						<Well>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi architecto maiores adipisci, accusamus aspernatur corrupti hic facilis iure assumenda at? Odit atque, dignissimos dolore modi temporibus error nobis saepe. Minus?
+						</Well>
+					</Collapse>
+				</ButtonGroup>
 			</Col>
 		)
 	}
