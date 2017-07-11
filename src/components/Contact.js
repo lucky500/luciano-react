1f24cce7adb4880ac402c25ecf5461a1a349d3e3
Luciano Oliveira <luciano101@msn.com>
2017-07-10 23:52:18 -0400

    finally was able to make map work

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import {Col} from 'react-bootstrap';
+import Data from '../data/data.json';
 
 export default props => (
 	<Col xs={12} md={6} lg={6} className="left-col">
 		Hello
 	</Col>
 );
+
+
+
+
+/*<Panel header={Data.projectLink.title} eventKey={Data.projectLink.event.key} key={Data.projectLink.event.key}>
+  <p>{Data.projectLink.description}</p>
+  <a href="#">{Data.projectLink.urlDemo}</a><br />
+  <a href="#">Data.projectLink.urlCode}</a>
+</Panel>*/
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,32 @@
 import React, {Component} from 'react';
 import {Col, Accordion, Panel, Well } from 'react-bootstrap';
+import Data from '../data/data.json';
 
 class Projects extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-
+			Data: Data
 		};
 	}
+
+	renderList(projectLinks){
+		let self = this;
+		return projectLinks.map(function(link) {
+			return <Panel header={link.title} eventKey={link.title} key={link.title}>
+	      			<p>{link.description}</p>
+	      			<a href="#">{link.urlDemo}</a><br />
+	      			<a href="#">{link.urlCode}</a>
+	    			</Panel>
+		});
+	}
+
 	render(){
 		return(
 			<Col xs={12} md={6} lg={6} className="right-col">
 				<Accordion>
-    <Panel header="Project #1" eventKey="1">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-    </Panel>
-    <Panel header="Project #2" eventKey="2">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-    </Panel>
-    <Panel header="Project #3" eventKey="3">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      	<Well><strong>used:</strong>HTML, CSS, JAVASCRIPT
-      	</Well>
-    </Panel>
-  </Accordion>
+					{this.renderList(Data.projectLinks)}
+  			</Accordion>
 			</Col>
 		)
 	}
