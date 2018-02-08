d9efe7222899d0f97c8ca346b2d566adce06c19b
Luciano Oliveira <luciano101@msn.com>
2018-02-07 23:18:21 -0800

    remove project because link issues

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Row } from 'reactstrap';
 
 import Contact from './components/Contact';
-//import Projects from './components/Projects';
-import Test from './components/Test';
 import './App.css';
 
 class App extends Component {
@@ -12,7 +10,6 @@ class App extends Component {
       <div className="App">
         <Row>
           <Contact />
-          <Test />
         </Row>
       </div>
     );
